29f21983ffeb19e42e9be499a8d2947c7c3397ef
iradz <irad.zehavi@outlook.com>
2023-05-18 08:47:20 +0000

    version 0.0.5

--- fastai_datasets/__init__.py
+++ fastai_datasets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 # Bugfix: fastprogress bars not showing in VSCode notebooks. Taken from https://github.com/microsoft/vscode-jupyter/issues/13163
 import IPython
